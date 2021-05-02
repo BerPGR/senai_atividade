49507b5c546a8a03e0078d08dca55a59d89f9f4b
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2021-05-02 10:28:00 -0300

    primeiro commit

---
+++ src/app/Program.java
@@ -0,0 +1,25 @@
+package app;
+
+public class Program {
+
+	public static void main(String[] args) {
+		 	mensagem1();
+		    System.out.println();
+		    mensagem2();
+		    System.out.println();
+		    mensagem3();
+		  }
+
+		  public static void mensagem1(){
+		    System.out.println("Olá pessoal do SENAI! Tudo bem?");
+		  }
+
+		  public static void mensagem2(){
+		    System.out.println("Esse daqui é um passo a passo de como criar e jogar projetos no github.");
+		  }
+
+		  public static void mensagem3(){
+		    System.out.println("Vejo vocês durante o curso!");
+		  }
+
+	}
